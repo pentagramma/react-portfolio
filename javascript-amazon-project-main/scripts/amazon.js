5614388ab5035a8e67bd765ef9322cec2e49ce09
SarthakProjects <famousfive.17@gmail.com>
2024-02-22 23:21:31 +0530

    interactive cart quantity

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -81,6 +81,12 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
+      let cartQuantity = 0;
+      cart.forEach((item)=> {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity')
+        .innerHTML= cartQuantity;
       
     });
   })
