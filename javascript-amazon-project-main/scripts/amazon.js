fcfac83d6602ab6c217786d23ca5bee4c9fe9b03
SarthakProjects <famousfive.17@gmail.com>
2024-02-23 09:21:34 +0530

    move addToCart in cart.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,5 @@
- import {cart} from '../data/cart.js';
+ import {cart,addToCart} from '../data/cart.js';
+ import { products } from '../data/products.js';
 
 let productsHTML = '';
 products.forEach((product) => {
@@ -57,7 +58,15 @@ products.forEach((product) => {
 
 });
 
-
+function updateCartQuantity()
+{
+  let cartQuantity = 0;
+  cart.forEach((cartItem)=> {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector('.js-cart-quantity')
+    .innerHTML= cartQuantity;
+}
 
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
@@ -66,32 +75,10 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-      let matchingItem;
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-      const quantity = Number(quantitySelector.value);
-
-      if (matchingItem) {
-        matchingItem.quantity += quantity;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: quantity
-        });
-      }
-
-      let cartQuantity = 0;
-      cart.forEach((item)=> {
-        cartQuantity += item.quantity;
-      });
-      document.querySelector('.js-cart-quantity')
-        .innerHTML= cartQuantity;
-      
+      addToCart(productId);
+      updateCartQuantity();
     });
   })
+
+      
+      
